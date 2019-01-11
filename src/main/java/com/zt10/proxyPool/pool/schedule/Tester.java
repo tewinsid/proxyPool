974a62208593621585d456acfc37b563d5cba464
tewinsid <vipwanggang@outlook.com>
2019-01-11 18:05:55 +0800

    1

--- src/main/java/com/zt10/proxyPool/pool/schedule/Tester.java
+++ src/main/java/com/zt10/proxyPool/pool/schedule/Tester.java
@@ -6,13 +6,15 @@ import com.zt10.proxyPool.utils.NetUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.*;
 
 @Component
 public class Tester {
-    private final String URL = "http://www.baidu.com";
+
 
     @Autowired
     public RedisOperation redisOperation;
@@ -41,26 +43,68 @@ public class Tester {
         }
     }
 
-    private void availableDetection(final String proxy,final String prefix) {
-        exec.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    String[] proxyMessage = proxy.split(":");
-                    boolean flag = NetUtil.get(URL, proxyMessage[0], proxyMessage[1]);
-                    if (flag) {
-                        redisOperation.put(proxy);
-                        System.out.println(prefix + "  ---  " + proxy + "  is available");
-                    } else {
-                        redisOperation.remove(proxy);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    throw new ThreadPoolException("");
-                }
-
-            }
-        });
+    private void availableDetection(final String proxy, final String prefix) {
+        exec.execute(new AvailableRunnable(proxy, prefix));
     }
 
+    /**
+     * 因为Spring本身默认Bean为单例模式构建，同时是非线程安全的，因此禁止了在Thread子类中的注入行为，
+     * 因此在Thread中直接注入的bean是null的，会发生空指针错误。
+     * 这里使用内部列形式访问外部类spring注入的redisOperation
+     * 感觉通过参数每次创建线程更新线程类变量的方式太脑残了
+     */
+    class AvailableRunnable implements Runnable {
+
+
+        private String proxy;
+
+        private String prefix;
+
+        private final String URL = "http://www.baidu.com";
+
+
+        public AvailableRunnable(String proxy, String prefix) {
+            this.setPrefix(prefix);
+            this.setProxy(proxy);
+        }
+
+        @Override
+        public void run() {
+            String proxy = this.getProxy();
+            String prefix = this.getPrefix();
+            String[] proxyMessage = proxy.split(":");
+            try {
+                boolean flag = NetUtil.get(URL, proxyMessage[0], proxyMessage[1]);
+                if (flag) {
+                    redisOperation.put(proxy);
+                    System.out.println(prefix + "  ---  " + proxy + "  is available");
+                } else {
+                    redisOperation.remove(proxy);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new ThreadPoolException("");
+            }
+        }
+
+
+        public synchronized String getProxy() {
+            return proxy;
+        }
+
+        public synchronized void setProxy(String proxy) {
+            this.proxy = proxy;
+        }
+
+        public synchronized String getPrefix() {
+            return prefix;
+        }
+
+        public synchronized void setPrefix(String prefix) {
+            this.prefix = prefix;
+        }
+
+    }
 }
+
+
